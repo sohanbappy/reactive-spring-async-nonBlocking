3e9c0b05bc6496fea1ffac2157904ff6c81326cf
SohanBappy <sohanbappy@gmail.com>
2021-10-23 04:51:44 +0600

    Lombok added,,Sample Stream added

--- src/main/java/com/reactivespring/ReactiveSpringApplication.java
+++ src/main/java/com/reactivespring/ReactiveSpringApplication.java
@@ -3,11 +3,22 @@ package com.reactivespring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class ReactiveSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactiveSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        //Stream, filter
+        List<String> list = Stream.of("abcd", "def", "rst").collect(Collectors.toList())
+                .stream().filter(e -> e.contains("d")).collect(Collectors.toList());
+        //forEach, Lambda
+        list.stream().forEach(e -> System.out.println(e.toUpperCase()));
+
+        SpringApplication.run(ReactiveSpringApplication.class, args);
+    }
 
 }
