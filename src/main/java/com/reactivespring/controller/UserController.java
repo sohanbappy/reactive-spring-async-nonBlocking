3ae0dab90da64203b05292ae14c29e2a393ad8a5
SohanBappy <sohanbappy@gmail.com>
2021-10-23 05:44:24 +0600

    Added produces parameter in the API-endPoint (TEXT_EVENT_STREAM_VALUE),,,And getting Non-blocking.

--- src/main/java/com/reactivespring/controller/UserController.java
+++ src/main/java/com/reactivespring/controller/UserController.java
@@ -3,6 +3,7 @@ package com.reactivespring.controller;
 import com.reactivespring.entity.User;
 import com.reactivespring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -19,7 +20,7 @@ public class UserController {
         return userService.getAllUser();
     }
 
-    @GetMapping(path = "/allUserStream")
+    @GetMapping(path = "/allUserStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<User> getAllUserStream() {
         return userService.getAllUserStream();
     }
